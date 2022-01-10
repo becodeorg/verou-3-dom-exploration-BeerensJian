3d9e91e5d4d92a12eea94ae266acfa71c5667cb8
JianB <Beerensjian@gmail.com>
2022-01-10 14:24:12 +0100

    Selector exercise loop through image display hidden and loop through paragraphs and display content + classname if there

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-const allItems = document.querySelectorAll('.important')
+const allItems = document.querySelectorAll('.important');
 for (let i = 0; i < allItems.length; i++) {
-    allItems[i].setAttribute("title", "this is an important item")
+    allItems[i].setAttribute("title", "this is an important item");
 }
 
-console.log(allItems[0].getAttribute('title'))
+console.log(allItems[0].getAttribute('title'));
+
+const allImages = document.getElementsByTagName("img");
+for (var i = 0; i < allImages.length; i++) {
+    if ( allImages[i].className != "important" ) {
+        allImages[i].style.display = "none" ;
+    }
+}
+
+
+const allPars = document.querySelectorAll('p');
+for ( i = 0; i < allPars.length; i++ ) {
+    if (allPars[i].className == "") {
+    console.log(allPars[i].innerText);
+} else {
+    var className = allPars[i].innerText.concat( " classname: ", allPars[i].className );
+    console.log(className);
+    }
+}
