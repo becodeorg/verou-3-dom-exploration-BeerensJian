04377fea72704164f3dcb7e0b9d8114ab3c0ea1c
JianB <Beerensjian@gmail.com>
2022-01-10 10:28:24 +0100

    Getchildren

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 console.log(document.title = "Modifying the DOMgit add .");
 document.body.style.backgroundColor = "rgb(255,100,0)"
+
+var childList = document.body.children;
+for (let i = 0; i < childList.length; i++ ) {
+    console.log(childList[i]);
+}
+
