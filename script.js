859e25286195a2be86a1a7fdc43aef9f468c78ae
JianB <Beerensjian@gmail.com>
2022-01-10 11:03:58 +0100

    Change title of all important elements

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-console.log(document.title = "Modifying the DOMgit add .");
-document.body.style.backgroundColor = "rgb(255,100,0)"
-
-var childList = document.body.children;
-for (let i = 0; i < childList.length; i++ ) {
-    console.log(childList[i]);
+const allItems = document.querySelectorAll('.important')
+for (let i = 0; i < allItems.length; i++) {
+    allItems[i].setAttribute("title", "this is an important item")
 }
 
+console.log(allItems[0].getAttribute('title'))
