efb29bcd98dd6c8ba767cb439b46f7b944a68851
JianB <Beerensjian@gmail.com>
2022-01-11 16:10:56 +0100

    Organized the code and created folders for a clear overview

--- creating-nodes/script.js
+++ creating-nodes/script.js
@@ -3,34 +3,28 @@ document.body.onload = addLearners();
 var bgColor = "hsl(211, 94%, 40%)";
 document.body.style.backgroundColor = bgColor;
 
-var learners1 = ["Jian", "Feruz", "Sara", "Ahmad", "Jawid", "Michelle", "Jordy", "Benjamin", "Dante"];
-var leaners2 = shuffle(learners1);
-console.log(leaners2)
-
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
-  }
-  
+}
+
 // function to generate a random HSL color
 function randomHSL() {
     var h = Math.round(Math.random() * 360);
     var s = Math.round(Math.random() * 100);
     var l = Math.round(Math.random() * 100);
     return "hsl(" + h + ", " + s + "%, " + l + "%)";
-    }
+}
 
 
-    // Function that turns the last part of a HSL color code into an Integer
-   function getLuminence(color) {
+// Function that turns the last part of a HSL color code into an Integer
+function getLuminence(color) {
     var x = color.slice(13);
-    x = x.replace(/\D/g,'');
+    x = x.replace(/\D/g, '');
     return parseInt(x);
-   }
-
-   
+}
 
 function addLearners() {
-    
+
     let learners = ["Jian", "Feruz", "Sara", "Ahmad", "Jawid", "Michelle", "Jordy", "Benjamin", "Dante"];
     learners = learners.sort(() => Math.random() - 0.5)
     for (i = 0; i < learners.length; i++) {
@@ -45,7 +39,7 @@ function addLearners() {
             newSection.style.color = "White";
         }
         newSection.style.backgroundColor = randomColor; // Give the section element a random background color
-        
+
         const currentArticle = document.querySelector("article"); // Select the article where the content is going to be added
         currentArticle.appendChild(newSection) //append the sections in the article element
 
