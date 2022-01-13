206754c452495e1d2ce11dcba771aea63664298b
JianB <Beerensjian@gmail.com>
2022-01-13 12:35:02 +0100

    Forgot to add stuff // same comment as last commit

--- Navigating-the-DOM/script.js
+++ Navigating-the-DOM/script.js
@@ -1,10 +1,11 @@
+// Put the last child of the ul before the first child
 const orderedList = document.querySelector("ol")
 const lastChild = orderedList.lastElementChild;
 const firstChild = orderedList.firstElementChild;
 console.log(lastChild)
 orderedList.insertBefore(lastChild, firstChild);
 
-// TODO: Move the <h2> of the third section in the second one and vice-versa
+// Move the <h2> of the third section in the second one and vice-versa
 
 const sections = document.querySelectorAll("section");
 console.log(sections);
@@ -13,3 +14,8 @@ const section3title = sections[2].firstElementChild.firstElementChild;
 sections[1].replaceChild(section3title, section2Title);
 const parento = sections[2].firstElementChild;
 parento.insertBefore(section2Title, parento.firstElementChild);
+
+// Delete the last section from the DOM, we don't need it anyways
+
+const body = document.querySelector("body");
+body.removeChild(body.children[2]);
