e2fc71bfd01daf40439ad342a72979eb985a35cd
JianB <Beerensjian@gmail.com>
2022-01-13 12:34:05 +0100

    Switch H2 elements in the second and third section, also removed the footer

--- Navigating-the-DOM/script.js
+++ Navigating-the-DOM/script.js
@@ -4,6 +4,12 @@ const firstChild = orderedList.firstElementChild;
 console.log(lastChild)
 orderedList.insertBefore(lastChild, firstChild);
 
+// TODO: Move the <h2> of the third section in the second one and vice-versa
 
-
-
+const sections = document.querySelectorAll("section");
+console.log(sections);
+const section2Title = sections[1].firstElementChild;
+const section3title = sections[2].firstElementChild.firstElementChild;
+sections[1].replaceChild(section3title, section2Title);
+const parento = sections[2].firstElementChild;
+parento.insertBefore(section2Title, parento.firstElementChild);
