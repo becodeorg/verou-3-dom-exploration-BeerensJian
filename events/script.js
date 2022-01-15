4352760f1340ec09c0163984985f9328e55c8a62
JianB <beerensjian@gmail.com>
2022-01-15 14:54:53 +0100

    Added switch instead of if else

--- events/script.js
+++ events/script.js
@@ -1,27 +1,30 @@
 const _initTime = Date.now()
 
 function getElapsedTime(){
-  return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
+    return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
 }
 
 function clickOnSquare(e){
-  console.log(e.target.classList[1]);
-  console.log(getElapsedTime());
-  const newDiv = document.createElement("div");
-  newDiv.className = "displayedsquare";
-  const wrapper = document.querySelector(".displayedsquare-wrapper");
-  if (e.target.classList[1] === "green") {
-      newDiv.classList.add("green")
-  } else if (e.target.classList[1] === "violet") {
-    newDiv.classList.add("violet")
-  } else {
-    newDiv.classList.add("orange") 
-  }
-  wrapper.appendChild(newDiv);
+    console.log(e.target.classList[1]);
+    console.log(getElapsedTime());
+    const newDiv = document.createElement("div");
+    newDiv.className = "displayedsquare";
+    const wrapper = document.querySelector(".displayedsquare-wrapper");
+    switch (e.target.classList[1]) {
+        case "green":
+            newDiv.classList.add("green")
+            break
+        case "violet":
+            newDiv.classList.add("violet")
+            break
+        case "orange":
+            newDiv.classList.add("orange")
+}
+wrapper.appendChild(newDiv);
 
 }
 
 const actionsquares = document.querySelectorAll('.actionsquare')
 for(let actionsquare of actionsquares){
-  actionsquare.addEventListener('click', clickOnSquare)
+actionsquare.addEventListener('click', clickOnSquare)
 }
