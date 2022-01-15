19669bdff251f02e44e787dcea60f65e8f8880a4
JianB <beerensjian@gmail.com>
2022-01-15 16:03:35 +0100

    When space is pressed add random gradient to background of body

--- events/script.js
+++ events/script.js
@@ -1,7 +1,7 @@
-const _initTime = Date.now()
+const _initTime = Date.now();
 
 function getElapsedTime(){
-    return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
+    return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's';
 }
 
 function clickOnSquare(e){
@@ -9,22 +9,45 @@ function clickOnSquare(e){
     console.log(getElapsedTime());
     const newDiv = document.createElement("div");
     newDiv.className = "displayedsquare";
-    const wrapper = document.querySelector(".displayedsquare-wrapper");
+    const wrapper = document.querySelector(".displayedsquare-wrapper");;
+    const list = document.querySelector("ul");
+    const newLi = document.createElement("li");
     switch (e.target.classList[1]) {
         case "green":
-            newDiv.classList.add("green")
+            newDiv.classList.add("green");
+            newLi.innerText = "[" + getElapsedTime() + "]" + " Created a new green square";
             break
         case "violet":
-            newDiv.classList.add("violet")
+            newDiv.classList.add("violet");
+            newLi.innerText = "[" + getElapsedTime() + "]" + " Created a new violet square";
             break
         case "orange":
-            newDiv.classList.add("orange")
+            newDiv.classList.add("orange");
+            newLi.innerText = "[" + getElapsedTime() + "]" +  " Created a new orange square";
 }
 wrapper.appendChild(newDiv);
+list.appendChild(newLi);
 
 }
 
-const actionsquares = document.querySelectorAll('.actionsquare')
+const actionsquares = document.querySelectorAll('.actionsquare');
 for(let actionsquare of actionsquares){
-actionsquare.addEventListener('click', clickOnSquare)
+actionsquare.addEventListener('click', clickOnSquare);
+}
+
+document.body.addEventListener("keypress", function(e){
+    if (e.code === "Space") {
+        document.body.style.background = randomgradient( );
+    }
+})
+
+function randomRGB() {
+    const r = Math.round(Math.random() * 255);
+    const g = Math.round(Math.random() * 255);
+    const b = Math.round(Math.random() * 255);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function randomgradient() {
+    return "linear-gradient( to bottom right," + randomRGB() + ", " + randomRGB() + ' ) fixed';
 }
