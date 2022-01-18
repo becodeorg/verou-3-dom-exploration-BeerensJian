f2ef2712d4f4bd49af6a18c1f063afbe716f38dd
JianB <Beerensjian@gmail.com>
2022-01-18 17:46:54 +0100

    Age check working and password veriffier does its job TODO: make it better

--- forms/script.js
+++ forms/script.js
@@ -5,8 +5,45 @@ const firstName = document.querySelector("#firstname");
 const displayName = () => {
     const inputvalue = firstName.value;
     const nameWrapper = document.querySelector("#display-firstname")
-    nameWrapper.innerHTML = inputvalue + " is cool";
+    nameWrapper.innerHTML = inputvalue;
 }
-
 firstName.addEventListener("keyup", displayName);
 
+//Add a keyup listener to the second inputfield
+// if not 18 dont show section if over 18 show section
+
+const ageInput = document.querySelector("#age");
+
+const oldEnough = () => {
+    if (ageInput.value >= 18) {
+        const truth = document.querySelector("#a-hard-truth")
+        truth.style.visibility = "visible"
+    } else {
+        const truth = document.querySelector("#a-hard-truth")
+        truth.style.visibility = "hidden"
+    }
+}
+ageInput.addEventListener("keyup", oldEnough);
+
+// password verifier
+// Add a keyup listener to both
+// Add a visualhint if passwords are not the same or smaller than 6 letters
+
+const passfield1 = document.querySelector("#pwd")
+const passfield2 = document.querySelector("#pwd-confirm")
+
+const passCheck = (e) => {
+
+    if (passfield1.value.length < 6 || passfield1.value !== passfield2.value) {
+        passfield1.style.backgroundColor = "red";
+        passfield2.style.backgroundColor = "red";
+    }
+    else {
+        passfield1.style.backgroundColor = "white";
+        passfield2.style.backgroundColor = "white";
+    }
+    console.log(e)
+}
+
+passfield1.addEventListener("keyup", passCheck)
+passfield2.addEventListener("keyup", passCheck)
